728be6a0faf1740c4865ea794ee16d0a8111021e
Brijbihari <ajitawadhiya@gmail.com>
2024-04-19 18:36:59 +0530

    changes

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
+
 export const YouTubeForm = () => {
   const form = useForm();
 
   const { register, control } = form;
   // register() for tracking our form state
   //   const { name, ref, onchange, onBlur } = register;
+
+  renderCount++;
   return (
     <>
+      <h1>Youtube Form ({renderCount / 2})</h1>
       <form>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
